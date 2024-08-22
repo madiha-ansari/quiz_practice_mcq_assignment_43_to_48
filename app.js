91bd301c1162199876b105bbb4ba6fdc3a5a12ab
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2024-08-22 07:49:30 +0500

    quiz practices assignment

---
+++ app.js
@@ -0,0 +1,17 @@
+// question-3
+// function deleteRow(row) {
+//     row.parentNode.parentNode.remove()
+// }
+
+// question-5
+// let count = 0;
+// function increase() {
+//     document.getElementById("counter").innerText = ++count
+// }
+// function decrease(){
+//     if(count>0){
+//         document.getElementById("counter").innerText= --count
+//     }else{
+//         alert("cart is empty")
+//     }
+// }
